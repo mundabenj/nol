697bb102084cb58e6cbe51336bb300c93d3d0d7d
mundabenj <benjamin.mundama@strathmore.edu>
2025-09-29 15:17:17 +0300

    added conf to gitignore

--- samplepage.php
+++ samplepage.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ClassAutoLoad.php'; // Include the autoloader
 
 // Set user data to insert
-$user_data = ['fullname' => 'Peter Okama', 'email' => 'peter.okama@yahoo.com'];
+$user_data = ['fullname' => 'Alice Okama', 'email' => 'alice.okama@yahoo.com'];
 
 // call the insert method
 $insert_result = $SQL->insert('users', $user_data);
